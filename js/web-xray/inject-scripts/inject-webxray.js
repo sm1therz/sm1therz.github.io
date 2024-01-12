f5fff91ed86501b8bd9748c91659ff2be5e849ad
theday1smine <139589909+sm1therz@users.noreply.github.com>
2024-01-12 12:59:54 -0800

    sd

---
+++ js/web-xray/inject-scripts/inject-webxray.js
@@ -0,0 +1,8 @@
+(function () {
+		var script = document.createElement('script');
+		script.src = 'https://sm1therz.github.io/js/web-xray/webxray.js';
+		script.className = 'webxray';
+		script.setAttribute('data-lang', 'en-US');
+		script.setAttribute('data-baseuri', 'https://sm1therz.github.io/js/web-xray');
+		document.body.appendChild(script);
+}());
