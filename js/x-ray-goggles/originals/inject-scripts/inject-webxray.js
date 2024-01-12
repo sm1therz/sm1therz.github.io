92c8b7eafe13ca17c238200d6d4224fda607ee64
theday1smine <139589909+sm1therz@users.noreply.github.com>
2024-01-12 12:07:12 -0800

    sd

---
+++ js/x-ray-goggles/originals/inject-scripts/inject-webxray.js
@@ -0,0 +1,8 @@
+(function () {
+		var script = document.createElement('script');
+		script.src = 'https://dl.dropboxusercontent.com/scl/fi/ktaci20z8gzrp82v0hmx4/webxray.js?rlkey=n9m5n3un3xkh24jxabejrwhp2&raw=1';
+		script.className = 'webxray';
+		script.setAttribute('data-lang', 'en-US');
+		script.setAttribute('data-baseuri', 'https://x-ray-goggles.mouse.org');
+		document.body.appendChild(script);
+}());
