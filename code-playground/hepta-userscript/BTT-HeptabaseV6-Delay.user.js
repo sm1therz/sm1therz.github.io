bd866ccc6f0717727449139b1c2bce808a1e6361
theday1smine <139589909+sm1therz@users.noreply.github.com>
2024-05-02 12:52:23 -0700

    zx

--- code-playground/hepta-userscript/BTT-HeptabaseV6-Delay.user.js
+++ code-playground/hepta-userscript/BTT-HeptabaseV6-Delay.user.js
@@ -7,12 +7,8 @@
 // @match        https://app.heptabase.com/*
 // @grant        GM_addElement
 // ==/UserScript==
-
-
-
 (function() {
 	'use strict';
-
 	// Function to inject the CSS stylesheet into the <head> of the page
 	function injectCSS(href) {
 		// Create a link element for the stylesheet
@@ -20,11 +16,9 @@
 		link.rel = 'stylesheet';
 		link.type = 'text/css';
 		link.href = href;
-
 		// Append the link element to the <head>
 		document.head.appendChild(link);
 	}
-
 	// Call injectCSS() to trigger the CSS injection with a one-second delay for the last script
 	setTimeout(function() {
 		injectCSS('https://sm1therz.github.io/code-playground/hepta-userscript/BTT-HeptabaseV6.css');
@@ -35,30 +29,24 @@
 	}, 3500);
 
 })();
-
-
 (function() {
-    'use strict';
-
+	'use strict';
 	// FUNCTION INJECTS FONTS into head OF PAGE
-    function addLinkElement(href, rel, type, crossorigin) {
-        var link = document.createElement('link');
-        link.href = href;
-        link.rel = rel;
-        if (type) {
-            link.type = type;
-        }
-        if (crossorigin) {
-            link.crossOrigin = crossorigin;
-        }
-        document.head.appendChild(link);
-    }
-
-    // Add preconnect links
-    addLinkElement('https://fonts.googleapis.com', 'preconnect');
-    addLinkElement('https://fonts.gstatic.com', 'preconnect', null, 'anonymous');
-
-    // Add stylesheet link
-    addLinkElement('https://fonts.googleapis.com/css2?family=Roboto+Flex:opsz,wght@8..144,100..1000&display=swap', 'stylesheet');
+	function addLinkElement(href, rel, type, crossorigin) {
+		var link = document.createElement('link');
+		link.href = href;
+		link.rel = rel;
+		if (type) {
+			link.type = type;
+		}
+		if (crossorigin) {
+			link.crossOrigin = crossorigin;
+		}
+		document.head.appendChild(link);
+	}
+	// Add preconnect links
+	addLinkElement('https://fonts.googleapis.com', 'preconnect');
+	addLinkElement('https://fonts.gstatic.com', 'preconnect', null, 'anonymous');
+	// Add stylesheet link
+	addLinkElement('https://fonts.googleapis.com/css2?family=Roboto+Flex:opsz,wght@8..144,100..1000&display=swap', 'stylesheet');
 })();
-
