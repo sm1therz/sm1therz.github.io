9db9fb67d53301962737092192ed949cc41c8f0d
theday1smine <139589909+sm1therz@users.noreply.github.com>
2024-07-02 14:47:10 -0700

    Search in page - original - works;

--- code-playground/BTT-Floating-Webview/js/search-in-page-1/js/search-in-page-1-final.js
+++ code-playground/BTT-Floating-Webview/js/search-in-page-1/js/search-in-page-1-final.js
@@ -1,8 +1,10 @@
 // SEARCH IN PAGE - FINAL
 
 
-// TODO: ON CLICK
+// TODO: Add Event Listener: Listen for the keydown event to detect CMD + F.
+// TODO: Prevent Default Action: Prevent the default action to avoid the beep.
 // TODO: Inject > STYLES
+// TODO: Inject > BUTTON W ATTRIBUTES
+// TODO: Inject > ATTRIBUTES to <body> 
 // TODO: Inject > JQUERY 
 // TODO: Inject > BUTTON
-// TODO: Inject > ATTRIBUTES to <body> 
