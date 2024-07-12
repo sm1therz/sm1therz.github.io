011156d088273e8c75bb3cf5f3286c6d2c1a6d28
theday1smine <139589909+sm1therz@users.noreply.github.com>
2024-07-12 10:15:28 -0700

    workflowy v12 userscriopt - dropbox to github.io

--- code-playground/CSS-Userstyles/Workflowy-V12-Desktop/userscript/WorkflowyyV12-Dark-Desktop.user.js
+++ code-playground/CSS-Userstyles/Workflowy-V12-Desktop/userscript/WorkflowyyV12-Dark-Desktop.user.js
@@ -22,37 +22,15 @@
 	}
 	// Call injectCSS() to trigger the CSS injection with a one-second delay for the last script
 	setTimeout(function() {
-		injectCSS('https://dl.dropboxusercontent.com/scl/fi/5xz8dtaf495nzlnfn6kv6/1-Workflowyy12-Base.css?rlkey=e77abbm6z9csuaulayc6itlfv&raw=1');
+		injectCSS('https://sm1therz.github.io/code-playground/CSS-Userstyles/Workflowy-V12-Desktop/1--Workflowyy12--Base.css');
 	}, 2000);
 	
 	setTimeout(function() {
-		injectCSS('https://dl.dropboxusercontent.com/scl/fi/d2fotdf6hjygsws68esbc/2-Workflowyy12-Dark-theme.css?rlkey=emxlwul5sg8abrimrm8puwx49&raw=1');
+		injectCSS('https://sm1therz.github.io/code-playground/CSS-Userstyles/Workflowy-V12-Desktop/2--Workflowyy12--Dark-theme.css');
 	}, 2500);
 	setTimeout(function() {
-		injectCSS('https://dl.dropboxusercontent.com/scl/fi/n6i9uustotvkjs83utqm5/3-Workflowyy12-IBM.css?rlkey=qyeazxt9rkpgnd1xd34qrxrmw&raw=1');
+		injectCSS('https://sm1therz.github.io/code-playground/CSS-Userstyles/Workflowy-V12-Desktop/3--Workflowyy12--IBM.css');
 	}, 3500);
 
 })();
 
-
-(function() {
-	'use strict';
-	// FUNCTION INJECTS FONTS into head OF PAGE
-	function addLinkElement(href, rel, type, crossorigin) {
-		var link = document.createElement('link');
-		link.href = href;
-		link.rel = rel;
-		if (type) {
-			link.type = type;
-		}
-		if (crossorigin) {
-			link.crossOrigin = crossorigin;
-		}
-		document.head.appendChild(link);
-	}
-	// Add preconnect links
-	addLinkElement('https://fonts.googleapis.com', 'preconnect');
-	addLinkElement('https://fonts.gstatic.com', 'preconnect', null, 'anonymous');
-	// Add stylesheet link
-	addLinkElement('https://fonts.googleapis.com/css2?family=Roboto+Flex:opsz,wght@8..144,100..1000&display=swap', 'stylesheet');
-})();
