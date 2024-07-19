58488e2837e4db033382e16fed07d2843a9d62ce
theday1smine <139589909+sm1therz@users.noreply.github.com>
2024-07-19 11:47:10 -0700

    Heptabase, ChatGPT, Raindrop CSS Updates

---
+++ code-playground/CSS-Userstyles/Raindrop/Userscripts/Inject-Raindrop-CSS-Inline--Preview.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         INJECT RAINDROP
+// @namespace    http://yournamespace.com
+// @version      1.0
+// @description  Injects Raindrop CSS
+// @author       RSM
+// @match        https://preview.systems
+// @match        https://api.raindrop.io
+// @match        https://s3.eu-central-1.wasabisys.com
+// @grant        GM_addElement
+// ==/UserScript==
+(function() {
+  let style = `<style>
+  
+/*IMAGES - HOSTED ON RAINDROP*/
+  
+  img {
+      max-width: 100%;
+      margin: 0 auto;
+      box-shadow: 0 0 0 400px hsla(0,0%,10%);
+  }
+  
+</style>`;
+
+  document.head.insertAdjacentHTML("beforeend", style);
+})();
