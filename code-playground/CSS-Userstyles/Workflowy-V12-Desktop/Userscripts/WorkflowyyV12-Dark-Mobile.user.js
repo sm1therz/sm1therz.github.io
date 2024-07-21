f30f1a2b30645cb2f3edcb3a46ad7c5f1f52fbd2
Smitherz <139589909+sm1therz@users.noreply.github.com>
2024-07-21 09:44:08 -0700

    workflowy - code blocks

--- code-playground/CSS-Userstyles/Workflowy-V12-Desktop/Userscripts/WorkflowyyV12-Dark-Mobile.user.js
+++ code-playground/CSS-Userstyles/Workflowy-V12-Desktop/Userscripts/WorkflowyyV12-Dark-Mobile.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         WORKFLOWYY-V12-Dark-Mobile
+// @name         WORKFLOWYY-V12-Dark-Desktop
 // @namespace    http://yournamespace.com
 // @version      1.0
 // @description  Injects V12 into workflowy.
@@ -10,7 +10,6 @@
 
 (function() {
 	'use strict';
-
 	// Function to inject the CSS stylesheet into the <head> of the page
 	function injectCSS(href) {
 		// Create a link element for the stylesheet
@@ -18,22 +17,20 @@
 		link.rel = 'stylesheet';
 		link.type = 'text/css';
 		link.href = href;
-
 		// Append the link element to the <head>
 		document.head.appendChild(link);
 	}
-
 	// Call injectCSS() to trigger the CSS injection with a one-second delay for the last script
 	setTimeout(function() {
-		injectCSS('https://dl.dropboxusercontent.com/scl/fi/27belqk9ycv8nh4rcvmxi/Workflowy-V10-BASE-STRIPPED.css?rlkey=c9pheq5k5h3p6zl4xrqxi32gv&raw=1');
-	}, 1000);
-
-	setTimeout(function() {
-		injectCSS('https://dl.dropboxusercontent.com/scl/fi/y378vmvj0zr1nkdzikx25/Workflowy-V10-DARK.css?rlkey=8dpgazm8lr1g2h0f9o3yq2qtk&raw=1');
-	}, 1500);
-
-	setTimeout(function() {
-		injectCSS('https://dl.dropboxusercontent.com/scl/fi/4hqu5psecgj9slkbn7uul/Workflowy-V10-Mobile-Overrides.css?rlkey=5uhy0uvw9z0koin1bn1mohwyl&raw=1');
+		injectCSS('https://sm1therz.github.io/code-playground/CSS-Userstyles/Workflowy-V12-Desktop/2--Workflowyy12--Dark-theme.css');
 	}, 2000);
+	setTimeout(function() {
+		injectCSS('https://sm1therz.github.io/code-playground/CSS-Userstyles/Workflowy-V12-Desktop/1--Workflowyy12--Base.css');
+	}, 3000);
+	
+	setTimeout(function() {
+		injectCSS('https://sm1therz.github.io/code-playground/CSS-Userstyles/Workflowy-V12-Desktop/3--Workflowyy12--IBM.css');
+	}, 4000);
+
 })();
 
