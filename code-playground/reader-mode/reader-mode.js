aa06db6014a37c7aef26797f948125b8fac66412
Smitherz <139589909+sm1therz@users.noreply.github.com>
2024-07-22 06:58:27 -0700

    reader-mode

---
+++ code-playground/reader-mode/reader-mode.js
@@ -0,0 +1,104 @@
+(function() {
+    'use strict';
+
+    var blockquotes = document.querySelectorAll('blockquote:not([class])');
+    blockquotes.forEach(function(blockquote) {
+        var div = document.createElement('div');
+        div.className = 'img-blockquote';
+        while (blockquote.firstChild) {
+            div.appendChild(blockquote.firstChild);
+        }
+        blockquote.parentNode.replaceChild(div, blockquote);
+    });
+
+})();
+
+setTimeout(function() {
+    'use strict';
+
+    var quotes = document.getElementsByClassName('quote');
+    Array.prototype.forEach.call(quotes, function(quote) {
+        var blockquote = document.createElement('blockquote');
+        blockquote.className = 'quote';
+        while (quote.firstChild) {
+            blockquote.appendChild(quote.firstChild);
+        }
+        quote.parentNode.replaceChild(blockquote, quote);
+    });
+
+}, 1000);
+
+
+
+
+(function() {
+  let style = `<style>
+  :root {
+		/*sizes*/
+	  --bodyFontSize:15px;
+		/*colors*/
+		--blockquoteBrdClr:hsla(0,0%,50%);
+		--blockquoteClr:hsla(0,0%,0%,.65)
+
+	}
+	html,
+	body {
+		padding:0px;
+		margin:0px;
+	}
+	* {
+		box-sizing:border-box;
+	}
+	body {  
+		display: block;  
+		padding-left: 20px;  
+		padding-right: 20px;  
+		margin: 0 auto;  
+		width: 800px; 
+		max-width: 100%;
+	} 
+	
+	body, 
+	blockquote,
+	p,
+	ul li,
+	ol li {
+		font-size:var(--bodyFontSize);
+		line-height:1.35;
+	}
+	
+	blockquote {
+	font-size: calc(var(--bodyFontSize) - 1px);
+	margin-left: 0px !important;
+	margin-right: 0px !important;
+	-webkit-text-stroke-width:.3px;
+	border-left:5px solid var(--blockquoteBrdClr);
+	color:var(--blockquoteClr);
+	}
+	
+	blockquote,
+	.img-blockquote {
+	padding-left:30px;
+	padding-right:10px;
+	padding-top:6px;
+	padding-bottom:6px;
+	}
+	
+	* {
+	font-family: -apple-system, BlinkMacSystemFont, Helvetica Neue, Arial, sans-serif !important;
+	}
+	
+	
+	i,em {
+	font-style:italic;
+	}
+	
+	p{
+	margin-top: 10px;,
+	margin-bottom: 10px;
+	}
+
+</style>`;
+
+  document.head.insertAdjacentHTML("beforeend", style);
+})();
