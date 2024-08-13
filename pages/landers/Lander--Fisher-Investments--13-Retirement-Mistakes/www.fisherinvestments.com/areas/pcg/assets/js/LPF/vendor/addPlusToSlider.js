0465e142087289e9dd077d8aad3940b74ec59885
theday1smine <139589909+sm1therz@users.noreply.github.com>
2024-08-12 21:29:12 -0700

    Fisher-lander-lead-magnet

---
+++ pages/landers/Lander--Fisher-Investments--13-Retirement-Mistakes/www.fisherinvestments.com/areas/pcg/assets/js/LPF/vendor/addPlusToSlider.js
@@ -0,0 +1,55 @@
+﻿function sliderMaxValuePlus() {
+    function updateTooltipContent() {
+        const sliderHandle = document.querySelector('.slider-handle.min-slider-handle');
+        const element = document.querySelector('.value-slider');
+        const toolTipValue = document.querySelector('.tooltip-inner');
+        const toolTipContent = toolTipValue?.textContent;
+        if (sliderHandle && toolTipValue && element) {
+            const dataAttributeValue = element.getAttribute('data-plus-flag');
+
+            if (dataAttributeValue !== null) {
+                const valueMax = parseInt(sliderHandle.getAttribute('aria-valuemax'));
+                const valueNow = parseInt(sliderHandle.getAttribute('aria-valuenow'));
+                const currentValueText = sliderHandle.getAttribute('aria-valuetext');
+
+                if (valueMax === valueNow) {
+                    try {
+                        if (toolTipContent.includes('+') === false) {
+                            sliderHandle.setAttribute('aria-valuetext', toolTipValue.textContent);
+                            toolTipValue.textContent = currentValueText.replace(/\+$/, '') + '+';
+                        }
+                    } catch (error) {
+                        console.error('Error occurred while updating attributes:', error);
+                    }
+                    return;
+                } else {
+                    try {
+                        if (toolTipContent.includes('+') === true) {
+                            sliderHandle.setAttribute('aria-valuetext', currentValueText.replace(/\+$/, ''));
+                            toolTipValue.textContent = currentValueText.replace(/\+$/, '');
+                        }
+                    } catch (error) {
+                        console.error('Error occurred while updating attributes:', error);
+                    }
+                    return;
+                }
+                
+            }
+        }
+    }
+
+    const callback = (mutationsList) => {
+        for (let mutation of mutationsList) {
+            if (mutation.type === 'childList') {
+                updateTooltipContent();
+            }
+        }
+    }
+
+    const observer = new MutationObserver(callback);
+    observer.observe(document.body, { childList: true, subtree: true });
+}
+
+window.addEventListener('load', function () {
+    sliderMaxValuePlus();
+});
---
+++ pages/landers/Lander--Fisher-Investments--13-Retirement-Mistakes/www.fisherinvestments.com/areas/pcg/assets/js/requiredcookies.js
@@ -0,0 +1,18 @@
+﻿(function ($) {
+    $(function () {
+        if (!areCookiesEnabled()) {
+            $("#divCookieRequired").show();
+        }
+
+        function areCookiesEnabled() {
+            try {
+                document.cookie = 't=1';
+                var cookiesEnabled = document.cookie.indexOf('t=') !== -1;
+                document.cookie = 't=1; expires=Thu, 01-Jan-1970 00:00:01 GMT';
+                return cookiesEnabled;
+            } catch (e) {
+                return false;
+            }
+        }
+    });
+})(window.jQuery);
