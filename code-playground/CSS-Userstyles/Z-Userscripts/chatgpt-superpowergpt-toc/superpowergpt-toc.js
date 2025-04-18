ff8d2016d77cfcdf6bc3363a6a32d24ab50503f5
theday1smine <139589909+sm1therz@users.noreply.github.com>
2025-04-18 15:36:39 -0700

    sdsd

--- code-playground/CSS-Userstyles/Z-Userscripts/chatgpt-superpowergpt-toc/superpowergpt-toc.js
+++ code-playground/CSS-Userstyles/Z-Userscripts/chatgpt-superpowergpt-toc/superpowergpt-toc.js
@@ -1,13 +1,3 @@
-// ==UserScript==
-// @name         ChatGPT TOC - SuperpowerGPT
-// @namespace    http://tampermonkey.net/
-// @version      2025-04-05
-// @description  try to take over the world!
-// @author       You
-// @match        https://chatgpt.com/*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=chatgpt.com
-// @grant        none
-// ==/UserScript==
 (() => {
 	let currentConversationId = null;
 	let minimapObserver = null;
