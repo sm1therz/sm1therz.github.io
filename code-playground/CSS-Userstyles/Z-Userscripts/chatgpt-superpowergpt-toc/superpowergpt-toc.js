5ec335fa94d258842a4868ab62563a624c5b478b
theday1smine <139589909+sm1therz@users.noreply.github.com>
2025-04-27 12:07:51 -0700

    sdssd

--- code-playground/CSS-Userstyles/Z-Userscripts/chatgpt-superpowergpt-toc/superpowergpt-toc.js
+++ code-playground/CSS-Userstyles/Z-Userscripts/chatgpt-superpowergpt-toc/superpowergpt-toc.js
@@ -5,7 +5,7 @@
 // @description  try to take over the world!
 // @author       You
 // @match        https://chatgpt.com/*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=chatgpt.com
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=chatgpt.com 
 // @grant        none
 // ==/UserScript==
 (() => {
