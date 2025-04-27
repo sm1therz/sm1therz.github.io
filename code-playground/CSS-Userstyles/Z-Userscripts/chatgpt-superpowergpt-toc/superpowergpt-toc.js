84908f6279b1bfa37bc725b369c8aa2875ed72f9
theday1smine <139589909+sm1therz@users.noreply.github.com>
2025-04-27 12:08:13 -0700

    sdd

--- code-playground/CSS-Userstyles/Z-Userscripts/chatgpt-superpowergpt-toc/superpowergpt-toc.js
+++ code-playground/CSS-Userstyles/Z-Userscripts/chatgpt-superpowergpt-toc/superpowergpt-toc.js
@@ -6,7 +6,7 @@
 // @author       You
 // @match        https://chatgpt.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=chatgpt.com 
-// @grant        none
+// @grant        none 
 // ==/UserScript==
 (() => {
 	let currentConversationId = null;
