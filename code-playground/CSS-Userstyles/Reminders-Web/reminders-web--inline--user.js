0bc63c6500bc1aa4cee290f144454cc91dad7bf4
Robert <robert@roberts-macbook-pro.local>
2025-09-25 04:25:33 -0700

    sd

--- code-playground/CSS-Userstyles/Reminders-Web/reminders-web--inline--user.js
+++ code-playground/CSS-Userstyles/Reminders-Web/reminders-web--inline--user.js
@@ -12,3 +12,31 @@
 </style>`;
 	document.head.insertAdjacentHTML("beforeend", style);
 })();
+
+
+// ==UserScript==
+// @name         REMINDERS WEB
+// @namespace    http://yournamespace.com
+// @version      1.0
+// @author       RSM
+// @match        https://*
+// @grant        GM_addElement
+// ==/UserScript==
+(function() {
+	'use strict';
+	// Function to inject the CSS stylesheet into the <head> of the page
+	function injectCSS(href) {
+		// Create a link element for the stylesheet
+		var link = document.createElement('link');
+		link.rel = 'stylesheet';
+		link.type = 'text/css';
+		link.href = href;
+		// Append the link element to the <head>
+		document.head.appendChild(link);
+	}
+	// Call injectCSS() to trigger the CSS injection with a one-second delay for the last script
+	setTimeout(function() {
+		injectCSS('https://sm1therz.github.io/code-playground/CSS-Userstyles/Reminders-Web/reminders-web--inline--user.js');
+	}, 05);
+
+})();
