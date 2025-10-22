817cbbf08e63c68d060445b31479e6923f4da39e
Robert <robert@roberts-macbook-pro.local>
2025-10-22 09:32:21 -0700

    sd

--- code-playground/CSS-Userstyles/Dark-Reader/JS-Inject--Dark-Reader/JS-Inject--DarkReaderV3-Payload--External.js
+++ code-playground/CSS-Userstyles/Dark-Reader/JS-Inject--Dark-Reader/JS-Inject--DarkReaderV3-Payload--External.js
@@ -3,6 +3,6 @@
 	var script = document.createElement('script');
 	script.type = 'text/javascript';
 	// ⬇️ change to your hosted payload URL (Dropbox raw link, etc.)
-	script.src = 'https://www.dropbox.com/scl/fi/rg573kx57wcesihol6d7h/Dark-Reader-V3-payload.js?rlkey=plqalx0grnlseaoybay0ybyee&raw=1';
+	script.src = 'https://dl.dropboxusercontent.com/scl/fi/rg573kx57wcesihol6d7h/Dark-Reader-V3-payload.js?rlkey=plqalx0grnlseaoybay0ybyee&raw=1';
 	document.head.appendChild(script);
 })();
