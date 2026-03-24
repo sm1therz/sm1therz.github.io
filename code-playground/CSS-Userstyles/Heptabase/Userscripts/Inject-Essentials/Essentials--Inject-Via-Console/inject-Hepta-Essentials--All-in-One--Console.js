f1aa2ae70f358911f9123034f76eca6e11bc23ff
Robert <robert@m1-macbook-pro.local>
2026-03-24 08:52:17 -0700

    sdsdsd

--- code-playground/CSS-Userstyles/Heptabase/Userscripts/Inject-Essentials/Essentials--Inject-Via-Console/inject-Hepta-Essentials--All-in-One--Console.js
+++ code-playground/CSS-Userstyles/Heptabase/Userscripts/Inject-Essentials/Essentials--Inject-Via-Console/inject-Hepta-Essentials--All-in-One--Console.js
@@ -417,3 +417,58 @@
     }
   }, 500);
 })();
+//! WHITEBOARD > INSTANCE TITLES
+(function() {
+  const LINK_ID = 'whiteboard-instance-titles';
+  const HREF = 'https://sm1therz.github.io/code-playground/CSS-Userstyles/Heptabase/css/individual-css/view-whiteboard-essentials.css';
+
+  function createStylesheetLink(id, href) {
+    const link = document.createElement('link');
+    link.id = id;
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
+    link.href = href;
+    document.head.appendChild(link);
+  }
+
+  function addStyle() {
+    if (!document.getElementById(LINK_ID)) {
+      createStylesheetLink(LINK_ID, HREF);
+    }
+  }
+
+  function removeStyle() {
+    const el = document.getElementById(LINK_ID);
+    if (el && el.parentNode) {
+      el.parentNode.removeChild(el);
+    }
+  }
+
+  function shouldApplyStyles() {
+    const href = window.location.href || '';
+    return (
+      href.includes('/whiteboard/')
+    );
+  }
+
+  function updateStyleForLocation() {
+    if (shouldApplyStyles()) {
+      addStyle();
+    } else {
+      removeStyle();
+    }
+  }
+
+  // Run once for the current URL
+  updateStyleForLocation();
+
+  // Watch for URL changes in this SPA
+  let lastHref = window.location.href;
+  setInterval(() => {
+    const currentHref = window.location.href;
+    if (currentHref !== lastHref) {
+      lastHref = currentHref;
+      updateStyleForLocation();
+    }
+  }, 500);
+})();
