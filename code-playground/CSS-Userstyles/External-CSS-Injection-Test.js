c4ffdd720081ca3b4ea8a4c6f680ab97964e9607
Smitherz <139589909+sm1therz@users.noreply.github.com>
2026-04-13 12:56:57 -0700

    sd

---
+++ code-playground/CSS-Userstyles/External-CSS-Injection-Test.js
@@ -0,0 +1,7 @@
+javascript: (function() {
+  var link = document.createElement('link');
+  link.rel = 'stylesheet';
+  link.type = 'text/css';
+  link.href = 'https://sm1therz.github.io/code-playground/CSS-Userstyles/Z-Userscripts/Inject-CSS-Methods/css/inject-dummy-styles.css';
+  (document.head || document.documentElement).appendChild(link);
+})();
